bdd9aabd140a5b72c94a0496a52e86bdd71c5832
Minji Han <gksalswl159@naver.com>
2025-05-21 20:56:30 -0700

    Added Exercise5.js

---
+++ js/Exercise5.js
@@ -0,0 +1,12 @@
+let firstName = "Minji"
+let marks = 50
+
+if(marks >= 80){
+    console.log(`${firstName}: A`)
+}else if(marks>=60){
+    console.log(`${firstName}: B`)
+}else if(marks>=40){
+    console.log(`${firstName}: C`)
+}else{
+    console.log(`${firstName}: No Grade`)
+}
